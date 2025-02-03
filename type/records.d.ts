a41eb7be89bd240228bbae20bacb42f1bbb2f84d
handoko <srabutdotcom@gmail.com>
2025-02-03 22:43:21 +0700

    add parseServerHello function

--- type/records.d.ts
+++ type/records.d.ts
@@ -1,4 +1,6 @@
 import { TLSPlaintext } from "../src/mod.ts";
+import { Handshake } from "../src/dep.ts";
+import { ClientHello } from "@tls/keyexchange";
 
 /**
  * Parses an array of bytes into a set of TLSPlaintext records.
@@ -6,3 +8,17 @@ import { TLSPlaintext } from "../src/mod.ts";
  * @returns A Set containing TLSPlaintext records parsed from the array.
  */
 export function parseRecords(array: Uint8Array): Set<TLSPlaintext>;
+
+/**
+ * Parses a ServerHello message from a TLS 1.3 handshake response.
+ *
+ * @param {Uint8Array} array - The raw TLS response containing the ServerHello message.
+ * @param {ClientHello} clientHello - The previously sent ClientHello message.
+ * @param {Uint8Array} clientPrivateKey - The private key used for key exchange.
+ * @returns {Handshake[]} An array of parsed Handshake messages from the decrypted response.
+ */
+export function parseServerHello(
+   array: Uint8Array, 
+   clientHello: ClientHello, 
+   clientPrivateKey: Uint8Array
+): Handshake[];
